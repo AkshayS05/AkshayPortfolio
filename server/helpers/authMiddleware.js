650053fd51775a20d7af014ebf85e43d73946e92
Akshay sharma <akshaysharma5432@gmail.com>
2025-02-17 02:17:41 -0500

    fix cookies error --2

--- server/helpers/authMiddleware.js
+++ server/helpers/authMiddleware.js
@@ -1,28 +1,8 @@
 const jwt = require("jsonwebtoken");
 
-// const authMiddleware = (req, res, next) => {
-//   const token = req.cookies.token;
-
-//   if (!token) {
-//     return res.status(401).json({ error: "Unauthorized: No token provided" });
-//   }
-
-//   try {
-//     const decoded = jwt.verify(token, "SECRET_KEY");
-//     console.log("Decoded token:", decoded);
-//     req.user = { id: decoded.id, role: decoded.role, email: decoded.email };
-//     next();
-//   } catch (error) {
-//     console.error("JWT verification error:", error);
-//     return res
-//       .status(401)
-//       .json({ error: "Unauthorized: Invalid token", details: error.message });
-//   }
-// };
-
 const authMiddleware = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+  const token = req.cookies.token;
+
   if (!token) {
     return res.status(401).json({ error: "Unauthorized: No token provided" });
   }
@@ -39,5 +19,25 @@ const authMiddleware = (req, res, next) => {
       .json({ error: "Unauthorized: Invalid token", details: error.message });
   }
 };
+
+// const authMiddleware = (req, res, next) => {
+//   const authHeader = req.headers["authorization"];
+//   const token = authHeader && authHeader.split(" ")[1];
+//   if (!token) {
+//     return res.status(401).json({ error: "Unauthorized: No token provided" });
+//   }
+
+//   try {
+//     const decoded = jwt.verify(token, "SECRET_KEY");
+//     console.log("Decoded token:", decoded);
+//     req.user = { id: decoded.id, role: decoded.role, email: decoded.email };
+//     next();
+//   } catch (error) {
+//     console.error("JWT verification error:", error);
+//     return res
+//       .status(401)
+//       .json({ error: "Unauthorized: Invalid token", details: error.message });
+//   }
+// };
 
 module.exports = authMiddleware;
