937daf90a6563d69792f43ad0c982684ccc91736
Akshay sharma <akshaysharma5432@gmail.com>
2025-02-17 02:45:13 -0500

    cookie issue --trye --4

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -39,17 +39,11 @@ const loginUser = async (req, res) => {
       "SECRET_KEY",
       { expiresIn: "7d" }
     );
-    res
-      .cookie("token", token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-      })
-      .json({
-        success: true,
-        message: "Logged in successfully",
-        user: { email: user.email, role: user.role, id: user._id },
-      });
+    res.cookie("token", token, { httpOnly: true, secure: true }).json({
+      success: true,
+      message: "Logged in successfully",
+      user: { email: user.email, role: user.role, id: user._id },
+    });
   } catch (err) {
     res.status(500).json({ error: "Server Error" });
   }
